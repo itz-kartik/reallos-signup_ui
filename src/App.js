e497a9ab5289180e63a8915c706145a402d8bde4
itz-kartik <itzkartikforu@gmail.com>
2020-12-13 12:12:45 +0530

    sign up ui code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import {TextField} from '@material-ui/core'; 
+import {Button} from '@material-ui/core'; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < div className="App">
+      
+      <img id='logo' src="icon1.png" alt='logo'></img>
+      <div>
+      <h1>Tell us about yourself ,</h1>
+      </div>
+
+    <div class='Textfield'>
+      <div id='t1'>
+      <TextField 
+        size='medium'
+        label='Name'
+        variant='outlined'
+        color='primary'
+        />
+      </div>
+
+      
+
+      <div id='t2'>
+      <TextField 
+        size='medium'
+        label='Email'
+        variant='outlined'
+        color='primary'
+      />
+      </div>
+
+      
+
+      <div id='t3'>
+      <TextField 
+        size='medium'
+        label='Phone'
+        variant='outlined'
+        color='primary'
+      />
+      </div>
+
+      
+
+      <div id='t4'>
+      <TextField 
+        size='medium'
+        label='Password'
+        variant='outlined'
+        color='primary'
+      />
+      </div>
+
+      
+
+      <div id='t5'>
+      <TextField 
+        size='medium'
+        label='Confirm Password'
+        variant='outlined'
+        color='primary'
+      />
+      </div>
     </div>
+    
+      
+      <div >
+      <Button id = 'button' variant="contained" color="primary">
+        Create My Account
+      </Button>
+      </div>
+
+</div>
   );
 }
 
